4240336ea9da2240f0e77510c1690be6747a036c
Aileen <acacayorin@gmail.com>
2015-10-20 13:57:00 -0700

    commit before switching to new branch mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
+      
       flash[:success] = "Signup successful! You are now logged in."
       redirect_to root_path
     else
